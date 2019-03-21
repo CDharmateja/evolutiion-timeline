5ff1816f9e55b1219b47a5fd87816d8a838bfc73
Ubuntu <ubuntu@ip-172-31-85-196.ec2.internal>
2019-03-21 12:41:14 +0000

    fix: Download images

--- src/images.py
+++ src/images.py
@@ -5,11 +5,13 @@ url = 'http://wellcometreeoflife.org/resources/tree-of-life-files/?sort=filetype
 
 res = requests.get(url)
 
-soup = bs4.BeautifulSoup(res.text)
+soup = bs4.BeautifulSoup(res.text, features="html.parser")
 
 links = soup.select('.cat')[0].select('li a')
 
-text = [links[i].text.strip() for i in range(0, len(links), 3)]
+texts = soup.select('.cat')[0].select('li p')
+
+text = [texts[i].text.strip() for i in range(0, len(texts), 2)]
 
 links = [links[i].get('href') for i in range(0, len(links), 3)]
 
@@ -18,4 +20,5 @@ n = len(links)
 url = 'http://wellcometreeoflife.org'
 
 for i in range(n):
-    open(text[i], 'wb').write(requests.get(url+links[i]).content)
+    print('Downloading', text[i])
+    open('../images/'+text[i], 'wb').write(requests.get(url+links[i]).content)
